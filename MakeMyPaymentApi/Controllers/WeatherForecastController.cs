2473a396ce5b3f34362b3be7a170fb09b7af3f63
riad <riad.nazrul@gmail.com>
2022-11-15 15:25:36 +0600

    add sln

--- MakeMyPaymentApi/Controllers/WeatherForecastController.cs
+++ MakeMyPaymentApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace MakeMyPaymentApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchingd"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
